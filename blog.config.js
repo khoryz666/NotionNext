0f69c3bbdbc7e8a801c9565c4173a2ca37bb576f
路红叶先生 <102358035+khoryz666@users.noreply.github.com>
2024-10-17 22:08:13 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '路红叶先生/HongYe/YZ', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '·Selamat datang ke bidangku·欢迎来到我的世界·Welcome to my dimension·', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://notion-next-zeta-three-85.vercel.app/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
