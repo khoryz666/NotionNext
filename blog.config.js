0aabb0b686ec9c416ae3ca45191edecaf6263649
路红叶先生 <102358035+khoryz666@users.noreply.github.com>
2024-10-17 17:14:15 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -24,8 +24,8 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'HongYe', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || 'Welcome to my dimension', // 作者简介
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '路红叶先生/HongYe/YZ', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || '·Selamat datang ke bidangku·欢迎来到我的世界·Welcome to my dimension·', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
